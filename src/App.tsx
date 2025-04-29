b80c66e9ed4b16476a450e2152211f453cfe4b8e
Sala Anil Kumar <anilkumarsala2003@gmail.com>
2025-04-29 13:04:08 +0530

    routing related bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AttendanceMarking } from './components/AttendanceMarking';
 import { QRCodeDisplay } from './components/QRCodeDisplay';
+import AdminLogin from './components/AdminLogin';
+import AdminDashboard from './components/AdminDashboard';
 
 function App() {
   return (
@@ -8,10 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<QRCodeDisplay />} />
         <Route path="/mark-attendance/:sessionId" element={<AttendanceMarking />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
